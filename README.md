daf5e6fb2e960722ecc1647c1f48aee3c61f77f9
Stephane Godin <pub.sg@free.fr>
2025-02-27 23:33:11 +0100

    added manual edit of GPS position for accessibility

--- README.md
+++ README.md
@@ -25,6 +25,14 @@ Feel free to open issues or pull requests ! We will be happy to review and merge
 ## Translations status
 
 
+## Funding
+
+This project is funded through [NGI0 Entrust](https://nlnet.nl/entrust), a fund established by [NLnet](https://nlnet.nl) with financial support from the European Commission's [Next Generation Internet](https://ngi.eu) program. Learn more at the [NLnet project page](https://nlnet.nl/project/BrailleRAP).
+
+[<img src="https://nlnet.nl/logo/banner.png" alt="NLnet foundation logo" width="20%" />](https://nlnet.nl)
+[<img src="https://nlnet.nl/image/logos/NGI0_tag.svg" alt="NGI Zero Logo" width="20%" />](https://nlnet.nl/entrust)
+
+
 # Building on Windows
 
 ## Prerequisites
--- src/pages/CityMap.js
+++ src/pages/CityMap.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect} from 'react'
+import { useContext, useState, useRef} from 'react'
 import AppContext from "../components/AppContext";
 import { MapContainer , TileLayer, Circle,  useMapEvents} from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
@@ -9,22 +9,92 @@ const greenOptions = { color: 'green' }
 const CityMap = () => {
     const {GetLocaleString} = useContext(AppContext);
     const { setImagePreview } = useContext(AppContext);
-    const [position, setPosition] = useState(null);
+    const mapref = useRef (null);
+    const [position, setPosition] = useState([51.505, -0.09]);
     const [radius, setRadius] = useState(200);
     const [building, setBuilding] = useState (true);
     const [footpath, setFootpath] = useState (false);
     const [polygon, setPolygon] = useState (false);
+    const [request, setRequest] = useState (false);
+    const [editLatitude, setEditLatitude] = useState (51);
+    const [editLongitude, setEditLongitude] = useState (0);
+    
+    
+    const getLatitude = () => {
+        return (editLatitude);
+    }
+    const getLongitude = () => {
+        return (editLongitude);
+    }
+    
+    const setLatitude = (elat) => {
+        setEditLatitude(elat);
 
-    const handleClick = (e) => {
-        setPosition(e.latlng);
-        console.log(e.latlng);
+        let lat = parseFloat(elat);
+        if (lat)
+        {
+            if (lat > -90 && lat < 90)
+            {
+                if (position)
+                {
+                    if (position.lng)
+                    {
+                        setPosition([lat, position.lng]);
+                        if (mapref)
+                            mapref.current.setView([lat, position.lng], mapref.current.getZoom());
+                    }
+                    else
+                        setPosition([lat, 0]);
+                }
+                else
+                {
+                    setPosition([lat, 0]);
+                }
+            }
+        }
+        
+    }
+
+    const setLongitude = (elon) => {
+        setEditLongitude(elon);
+        let lon = parseFloat(elon);
+        if (lon)
+        {
+            if (lon > -180 && lon < 180)
+            {
+                if (position)
+                {
+                    if (position.lat){
+                        setPosition([position.lat, lon]);
+                        if (mapref)
+                            mapref.current.setView([position.lat, lon], mapref.current.getZoom());
+                    }
+                    else
+                        setPosition([0, lon]);
+
+                }
+                else
+                {
+                    setPosition([0, lon]);
+                }
+            }
+        }
     }
     const LocationFinder= () => {
         const map = useMapEvents({
             click(e) {
                 console.log(e.latlng);
+                // save position value
                 setPosition(e.latlng);
+                
+                // center map on click position
                 map.setView(e.latlng, map.getZoom())
+                
+                // report value in edit
+                console.log (typeof (e.latlng));
+                
+                setEditLatitude(e.latlng.lat);
+                setEditLongitude(e.latlng.lng);
             },
         });
         return null;
@@ -37,25 +107,26 @@ const CityMap = () => {
                     <h2>{position.lat} {position.lng}</h2>
                 );    
             
-            return (<h2>Click on the map to get the position</h2>);
+            
         }
+        /*
         const MapPosition = () => {
             if (position)
                 return [position.lat, position.lng];
             console.log("MapPosition default");
             return [51.505, -0.09];
         }
+            */
         const goRender = () =>
         {
             if (position)
             {
-                console.log ("footpath", footpath);
-                console.log ("building", building);
-                console.log ("polygon", polygon);
+                setRequest(true);
+
                 window.pywebview.api.ReadStreetMapData(position.lat, position.lng, radius, building, footpath, polygon).then ((svg) => {
                    
                     setImagePreview (svg);
-        
+                    setRequest(false);
                     
                 });
             }
@@ -99,7 +170,7 @@ const CityMap = () => {
                                 {GetLocaleString("citymap.streetpolygon")}
                                 </label>
 
-                            <button onClick={goRender}>
+                            <button disabled={request} onClick={goRender}>
                                 {GetLocaleString("citymap.rendermap")}
                             </button>
                         </div>
@@ -109,13 +180,25 @@ const CityMap = () => {
         }
     return (
         <>
+            <div className='CityMapParam'>
             <h1>{GetLocaleString("citymap.maptitle")}   </h1>
-            {renderPosition()}
-            <label>radius
-            <input type="number" value={radius} onChange={(e) => setRadius(e.target.value)} />
-            </label>
-            <MapContainer center={MapPosition()} zoom={1} scrollWheelZoom={true}
-            
+                {renderPosition()}
+                <label>{GetLocaleString("citymap.latitude")}
+                <input type="number" value={editLatitude} step="0.0001" min="-90" max="90" 
+                onChange={(e) => setLatitude(e.target.value)} />
+                </label>
+                <label>{GetLocaleString("citymap.longitude")}
+                <input type="number" value={editLongitude} step="0.0001" min="-180" max="180"
+                onChange={(e) => setLongitude(e.target.value)} />
+                </label>
+                <label>{GetLocaleString("citymap.radius")}
+                <input type="number" value={radius} onChange={(e) => setRadius(e.target.value)} />
+                </label>
+                
+                <h2>Click on the map to get the position</h2>
+            </div>
+            <MapContainer center={position} zoom={20} scrollWheelZoom={true}
+            ref={mapref}
             style={{ width: '99%', position: 'relative', zIndex: '9', height: '40vh' }}
             attributionControl={false}
             >
