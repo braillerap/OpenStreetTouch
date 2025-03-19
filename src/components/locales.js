10b07c056e0dcc8771e3862ed2f0c84774789fb1
Stephane Godin <pub.sg@free.fr>
2025-03-19 20:43:20 +0100

    added NL in translation

--- src/components/locales.js
+++ src/components/locales.js
@@ -1,12 +1,11 @@
 import French from '../locales/fr.json';
 import English from '../locales/en.json';
+import Netherland from '../locales/nl.json';
 
 const locales = {
-    
-    
-    
-    "fr":       {lang:"fr", dir:'ltr', desc:'fr - Français',data:French},
     "en":       {lang:"en", dir:'ltr', desc:'en - English',data:English},
+    "fr":       {lang:"fr", dir:'ltr', desc:'fr - Français',data:French},
+    "nl":       {lang:"nl", dir:'ltr', desc:'nl - Dutch',data:Netherland},
 }; 
 
 export default locales;
