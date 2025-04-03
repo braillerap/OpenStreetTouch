443037248134ba7ba79cf2bd476682c9dd7fdb85
Stephane Godin <pub.sg@free.fr>
2025-04-03 21:43:28 +0200

    update version

--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -59,9 +59,9 @@ copyright = u'GPL V3'
 author = u'BrailleRAP team'
 
 # The short X.Y version
-version = u'0.2.4'
+version = u'0.2.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.4'
+release = u'0.2.5'
 
 
 # -- General configuration ---------------------------------------------------
