e8c4a7ab4640c83b799303cd3b9c4949241da981
Stephane Godin <pub.sg@free.fr>
2025-04-08 21:49:59 +0200

    add simplified chinese in locale list

--- src/components/locales.js
+++ src/components/locales.js
@@ -1,11 +1,13 @@
 import French from '../locales/fr.json';
 import English from '../locales/en.json';
 import Netherland from '../locales/nl.json';
+import SimpleChineese from '../locales/zh-hans.json';
 
 const locales = {
     "en":       {lang:"en", dir:'ltr', desc:'en - English',data:English},
     "fr":       {lang:"fr", dir:'ltr', desc:'fr - Français',data:French},
     "nl":       {lang:"nl", dir:'ltr', desc:'nl - Dutch',data:Netherland},
+    "zh-hans":  {lang:"zh-hans", dir:'ltr', desc:'zh-hans - 简体中文',   reverse:false, data:SimpleChineese}
 }; 
 
 export default locales;
