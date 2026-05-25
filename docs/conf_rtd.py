4eaad42cad40cad40be085f6c8114259b9dad518
Stephane <pub.sg@free.fr>
2026-05-25 20:18:14 +0200

    chore: update version

--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -59,9 +59,9 @@ copyright = u'GPL V3'
 author = u'BrailleRAP team'
 
 # The short X.Y version
-version = u'0.2.5'
+version = u'0.2.7'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.5'
+release = u'0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
